86f0ae3e8e4c7477a836c7bd740953785a527865
Victoria Sánchez <vsanchezailen@gmail.com>
2023-12-10 19:23:36 -0300

    ADD changes

--- styles.js
+++
@@ -1,4 +0,0 @@
-function cambiarEstilos() {
-    // Cambia la hoja de estilo cuando se hace clic en el botón
-    document.querySelector('link[href="./css/estilos-futuro.css"]').href = 'nuevos-estilos.css';
-}
